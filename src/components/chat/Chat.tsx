6d2b3c07213534dd1b9559fb9142c8c19fd02c38
mitsuun <lounge.attachment@gmail.com>
2024-09-29 08:30:02 +0900

    firebaseを用いたバックエンドの実装

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,30 +1,93 @@
-import React from 'react';
+/** @format */
+
+import React, { useState } from "react";
 import "./Chat.scss";
-import ChatHeader from './ChatHeader';
+import ChatHeader from "./ChatHeader";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
 import GifIcon from "@mui/icons-material/Gif";
 import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
-import ChatMessage from './ChatMessage';
+import ChatMessage from "./ChatMessage";
+import { useAppSelector } from "../../app/hooks";
+import {
+  addDoc,
+  collection,
+  CollectionReference,
+  DocumentData,
+  serverTimestamp,
+} from "firebase/firestore";
+import { db } from "../../firebase";
+import useSubCollection from "../../hooks/useSubCollection";
+
+
 
 const Chat = () => {
+  const [inputText, setInputText] = useState<string>("");
+  const channelId = useAppSelector((state) => state.channel.channelId);
+  const channelName = useAppSelector((state) => state.channel.channelName);
+  const user = useAppSelector((state) => state.user.user);
+  const { subDocuments: messages } = useSubCollection("channels", "messages");
+  
+  const sendMessage = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    // channelsコレクションの中にあるmessagesコレクションの中にメッセージ情報を保存する
+    const collectionRef: CollectionReference<DocumentData> = collection(
+      db,
+      "channels",
+      String(channelId),
+      "messages"
+    );
+
+    await addDoc(
+      collectionRef,
+      {
+        message: inputText,
+        timestamp: serverTimestamp(),
+        user: user,
+      }
+    );
+    setInputText("");
+  };
+
   return (
     <div className="chat">
       {/* chatHeader */}
-      <ChatHeader />
+      <ChatHeader channelName={channelName} />
       {/* chathMessage */}
       <div className="chatMessage">
+        {messages.map((message, index) => (
+          <ChatMessage
+            key={index}
+            message={message.message}
+            timestamp={message.timestamp}
+            user={message.user}
+          />
+        ))}
+
+        {/* <ChatMessage />
         <ChatMessage />
-        <ChatMessage />
-        <ChatMessage />
-        <ChatMessage />
+        <ChatMessage /> */}
       </div>
       {/* chatInput */}
       <div className="chatInput">
         <AddCircleOutlineIcon />
         <form>
-          <input type="text" placeholder="#Udemyへメッセージを送信" />
-          <button type="submit" className="chatInputButton">
+          <input
+            type="text"
+            placeholder="#Udemyへメッセージを送信"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setInputText(e.target.value)
+            }
+            value={inputText}
+          />
+          <button
+            type="submit"
+            className="chatInputButton"
+            onClick={sendMessage}
+          >
             送信
           </button>
         </form>
@@ -37,6 +100,6 @@ const Chat = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Chat;
